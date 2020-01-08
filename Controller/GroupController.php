a18e2c11013b955726f8d65e08d551f3840d4e2f
XWB <karelke@gmail.com>
2020-01-08 19:33:07 +0100

    Do not change GroupController as it will be removed

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -19,7 +19,7 @@ use FOS\UserBundle\Form\Factory\FactoryInterface;
 use FOS\UserBundle\FOSUserEvents;
 use FOS\UserBundle\Model\GroupInterface;
 use FOS\UserBundle\Model\GroupManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,7 +34,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  *
  * @final
  */
-final class GroupController extends AbstractController
+class GroupController extends Controller
 {
     private $eventDispatcher;
     private $formFactory;
